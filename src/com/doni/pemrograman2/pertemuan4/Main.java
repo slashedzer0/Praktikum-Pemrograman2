25fa26f34eafa418c0882cefc8194c4b733c2bd5
slashedzer0 <21104062@ittelkom-pwt.ac.id>
2022-12-23 11:16:17 +0700

    Update pertemuan 7

--- src/com/doni/pemrograman2/pertemuan4/Main.java
+++ src/com/doni/pemrograman2/pertemuan4/Main.java
@@ -1,13 +1,13 @@
 package com.doni.pemrograman2.pertemuan4;
 
 public class Main {
-   public static void main(String[] args) {
+    public static void main(String[] args) {
 
-      Mobil mb = new Mobil("VW Beetle", 4, 2, "Pertamax");
+        Mobil mb = new Mobil("VW Beetle", 4, 2, "Pertamax");
 
-      mb.showInfo();
-      mb.nyalakanMesin();
-      mb.belok("Kanan");
-      mb.belok();
-   }
+        mb.showInfo();
+        mb.nyalakanMesin();
+        mb.belok("Kanan");
+        mb.belok();
+    }
 }
--- src/com/doni/pemrograman2/pertemuan7/Registration.java
+++ src/com/doni/pemrograman2/pertemuan7/Registration.java
@@ -1,6 +1,9 @@
 package com.doni.pemrograman2.pertemuan7;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Enumeration;
 
 public class Registration extends JFrame {
     private JTextField isiNamaLengkap;
@@ -29,6 +32,7 @@ public class Registration extends JFrame {
     private JLabel alamatLabel;
     private JLabel skillLabel;
     private JPanel mainPanel;
+    private ButtonGroup jenisKelamin;
 
 
     public Registration() {
@@ -37,6 +41,40 @@ public class Registration extends JFrame {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setContentPane(mainPanel);
         this.setSize(800, 600);
+
+        submitButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // mengambil nilai dari textField
+                String namaLengkap = isiNamaLengkap.getText();
+
+                // cara alternatif
+                String jKelamin = "";
+                Enumeration<AbstractButton> buttons = jenisKelamin.getElements();
+
+                for (int i = 0; i < jenisKelamin.getButtonCount(); i++) {
+                    AbstractButton button = buttons.nextElement();
+                    if (button.isSelected())
+                        jKelamin = button.getText();
+                }
+
+                // mengambil nilai dari ComboBox / dropdown
+                String jabatan = (String) pilihJabatan.getSelectedItem();
+
+                // mengambil nilai dari Spinner
+                int tahun = (int) pilihTahun.getValue();
+
+                // mengatur nilai min an max sebuah slider (default 0 - 100)
+                aturLevelKeahlian.setMinimum(50);
+                aturLevelKeahlian.setMaximum(200);
+
+                // mengambil nilai dari Slider
+                int skill = aturLevelKeahlian.getValue();
+
+                // menampilkan pesan
+                JOptionPane.showMessageDialog(null, "Data " + namaLengkap + " tersimpan!");
+            }
+        });
     }
 
     public static void main(String[] args) {
